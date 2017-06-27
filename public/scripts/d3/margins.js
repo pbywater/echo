9aab4421127811e9a9af4f90f9cbe39b2efc7fa0
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-06-27 10:28:11 +0100

    updated PR as per des-des comments placed global variables in iife in margin js removed comment specified css and removed svg renamed d3 canvas element relates #1 #3

--- public/scripts/d3/margins.js
+++ public/scripts/d3/margins.js
@@ -1,14 +1,15 @@
 /* eslint-disable */
 
-var width = 1000;
-var height = 1000;
+(function createSVG(){
+  var width = 1000;
+  var height = 1000;
 
-var svg = d3.select("#d3-canvas")
-  .append("svg")
-    // .attr("background-color", "pink")
-    .attr("width", "100%")
-    .classed("d3-canvas", true)
-    .attr("overflow", "visible")
-    .attr("height", "100%")
-  .append("g")
-    .attr("transform", "translate(0,0)")
+  var svg = d3.select("#d3-container")
+    .append("svg")
+      .attr("width", "100%")
+      .classed("d3-background", true)
+      .attr("overflow", "visible")
+      .attr("height", "100%")
+    .append("g")
+      .attr("transform", "translate(0,0)");
+})()
