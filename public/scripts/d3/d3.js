b623db4ca1f204d9f893a32ee66e4e21226e649a
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-06-27 12:33:30 +0100

    seperated nodes by tag we arranged the nodes on the page based on tag relates #3

--- public/scripts/d3/d3.js
+++ public/scripts/d3/d3.js
@@ -1,5 +1,23 @@
 d3.json(jsonUrl, function(err, data) {
 
+  var tags = [];
+  data.forEach(function(d){
+    tags.push(d.tag)
+  })
+  var uniqueTags = tags.filter(onlyUnique);
+  var nodePosition = [];
+  var cx = 0;
+  var cy = 0;
+  uniqueTags.forEach(function(t){
+    var tempObj = {}
+    tempObj.tag = t;
+    tempObj.cx = cx;
+    tempObj.cy = cy;
+    cx += 50;
+    cy += 50;
+    nodePosition.push(tempObj)
+  })
+
   var rScale = d3.scaleSqrt()
     .domain([0, d3.max(data, d => d.likes)])
     .range([0, 30]);
@@ -14,12 +32,39 @@ d3.json(jsonUrl, function(err, data) {
       })
       .attr('class', 'memoryG')
 
+var startCy = 0;
+var startCx = 0;
+
   circles
     .append('circle')
-      .attr('cy', 0)
-      .attr('cx', 0)
+      .attr('cy', (d, i) => {
+        if (i > 0) {
+          var prevData = circles.data()[i-1];
+          if (d.tag === prevData.tag){
+            return startCy;
+          } else {
+            startCy += 100;
+            return startCy;
+          }
+        }
+      })
+      .attr('cx', (d, i) => {
+        if (i > 0) {
+          var prevData = circles.data()[i-1];
+          if (d.tag === prevData.tag){
+            return startCx;
+          } else {
+            startCx += 100;
+            return startCx;
+          }
+        }
+      })
       .attr('class', 'memory')
       .attr('r', d => rScale(d.likes))
       .style('fill', 'white');
 
 })
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
