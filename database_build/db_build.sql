6ff807d0eeec1040f51ee227a8a7b0f7421bafed
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-06-30 10:54:40 +0100

    sorted out whitespace cleaned up whitespace in sql file relates #38

--- database_build/db_build.sql
+++ database_build/db_build.sql
@@ -3,7 +3,7 @@ BEGIN;
 DROP TABLE IF EXISTS memories, users, media CASCADE;
 
 CREATE TABLE memories (
-  id          SERIAL        PRIMARY KEY,
+  id          SERIAL          PRIMARY KEY,
   heading     VARCHAR(50)     NOT NULL,
   likes       INTEGER,
   rating      INTEGER,
@@ -12,17 +12,17 @@ CREATE TABLE memories (
 );
 
 CREATE TABLE users (
-  id          SERIAL        PRIMARY KEY,
-  username    VARCHAR(64)   UNIQUE NOT NULL,
-  password    VARCHAR(64)  NOT NULL,
-  email    VARCHAR(64)  NOT NULL
+  id          SERIAL          PRIMARY KEY,
+  username    VARCHAR(64)     UNIQUE NOT NULL,
+  password    VARCHAR(64)     NOT NULL,
+  email       VARCHAR(64)     NOT NULL
 );
 
 CREATE TABLE media (
-  text_only    TEXT[],
-  audio    TEXT[],
-  video    TEXT[],
-  image    TEXT[]
+  text_only   TEXT[],
+  audio       TEXT[],
+  video       TEXT[],
+  image       TEXT[]
 );
 
 INSERT INTO users (username, password, email)
