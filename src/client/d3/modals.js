f7a0d386480a7aa86bf00d6b1837f5635474522e
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-04 17:37:17 +0100

    fiddling with numbers relates #50

--- src/client/d3/modals.js
+++ src/client/d3/modals.js
@@ -5,19 +5,20 @@ const appendPopUp = (data) => {
   const holder = svg
     .append('g')
       .attr('class', 'popupBoxHolder')
-      .attr('transform', 'translate(0, 0)')
+      .attr('transform', 'translate(30, 50)') //change
 
   holder
     .append('rect')
       .attr('class', 'popupBox')
       .attr('fill', 'white') // change
       .attr('display', 'block')
-      .attr('width', 180) // change
-      .attr('height', 180) // change
+      .attr('width', 300) // change
+      .attr('height', 500) // change
 
   const textholder = holder
     .append('text')
       .attr('class', 'popupTextHolder')
+      .attr('transform', 'translate(10, 10)')
       .text(data.tag) // change
 
   const close = holder // all of this will change
