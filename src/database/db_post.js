c5861cb9e77180af4547eb7b15b6424e24a6c66d
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-04 15:24:51 +0100

    added register user post func set up so that when we implement sign up relates #48 #41

--- src/database/db_post.js
+++ src/database/db_post.js
@@ -1,5 +1,7 @@
 const connect = require('./db_connect');
 
+const hashPassword = request('./../helpers/hashPassword');
+
 const post = {};
 
 post.memory = (newMemory, callback) => {
@@ -14,15 +16,29 @@ post.memory = (newMemory, callback) => {
     UPDATE media SET
       ${newMemory.mediaType} = ${newMemory.mediaType} || (SELECT id FROM new_memory)`,
     [newMemory.username, newMemory.heading, newMemory.tag, newMemory.memory], (err, res) => {
-      if (err) { console.log(err); }
+      if (err) { callback(err); }
       callback(null, res);
     });
 };
 
-// post.registerUser = (userDetails, callback) => {
-//
-// };
+post.registerUser (userDetails, callback) => {
+  connect.query(
+    'SELECT username FROM users WHERE username = $1;', [userDetails.username], (err, res) => {
+      if (err) { return callback(err); }
 
+      if (!res.rows[0]) {
+        hashPassword(userDetails.password, (err, hash) => {
+          if (err) { return callback(err); }
 
-    // UPDATE media SET ${newMemory.mediaType} = array_append(${newMemory.mediaType}, (SELECT id FROM new_memory))`, [newMemory.username, newMemor
+          connect.query('INSERT INTO users (username, password, email) VALUES($1, $2, $3);', [userDetails.username, hash, userDetails.email], (err) => {
+            if (err) { return callback(err); }
+            callback(null, 'new user added');
+          });
+        })
+      } else {
+        callback(new Error('Sorry, that username is taken.'));
+      }
+    });
+};
+
 module.exports = post;
