68c221300272e95403f24b5d57ea1a9fd8d21cc7
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-05 10:43:32 +0100

    removed sql injection relates #41

--- src/database/db_create.js
+++ src/database/db_create.js
@@ -12,8 +12,8 @@ const createMemory = (newMemory, callback) => {
       RETURNING id
     )
     UPDATE media SET
-      ${newMemory.mediaType} = ${newMemory.mediaType} || (SELECT id FROM new_memory)`,
-    [newMemory.username, newMemory.heading, newMemory.tag, newMemory.memory], (err, res) => {
+      $5 = $5 || (SELECT id FROM new_memory)`,
+    [newMemory.username, newMemory.heading, newMemory.tag, newMemory.memory, newMemory.mediaType], (err, res) => {
       if (err) { callback(err); }
       callback(null, res);
     });
@@ -21,14 +21,19 @@ const createMemory = (newMemory, callback) => {
 
 const createUser (userDetails, callback) => {
   connect.query(
-    'SELECT username FROM users WHERE username = $1;', [userDetails.username], (err, res) => {
+    `SELECT username
+    FROM users
+    WHERE username = $1;`, [userDetails.username], (err, res) => {
       if (err) { return callback(err); }
 
       if (!res.rows[0]) {
         hashPassword(userDetails.password, (err, hash) => {
           if (err) { return callback(err); }
 
-          connect.query('INSERT INTO users (username, password, email) VALUES($1, $2, $3);', [userDetails.username, hash, userDetails.email], (err) => {
+          connect.query(
+            `INSERT INTO users
+            (username, password, email)
+            VALUES($1, $2, $3);`, [userDetails.username, hash, userDetails.email], (err) => {
             if (err) { return callback(err); }
             callback(null, 'new user added');
           });
