2f5f2da3c977f775ec8ed8c172fc8eec55d1b01f
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-06 16:46:46 +0100

    Refactor menu functions in main.js Remove duplicate tape dependency Relates #15

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,84 +1,53 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  $('#pencil').on('click', () => {
-    console.log('clicked');
-    $('.menu-open:checked ~ #pencil').removeClass('normal').addClass('wipe');
+  const menuItemIds = [
+    'microphone',
+    'camera',
+    'cog',
+    'pencil',
+    'clock',
+  ];
+
+  const getRestSelector = function (menuItemIdToFilter) {
+    return menuItemIds
+      .filter(menuItemId => menuItemId !== menuItemIdToFilter)
+      .map(menuItemId => `#${menuItemId}`)
+      .join(', ');
+  };
+
+  function setMenuItemClick(menuItemId) {
+    const restSelector = getRestSelector(menuItemId);
+
     setTimeout(() => {
-      $('.menu-open:checked ~ #pencil').addClass('finished');
-      $('.menu-open-button').fadeIn().addClass('active');
+      $(`.menu-open:checked ~ #${menuItemId}`)
+        .addClass('finished');
+      $('.menu-open-button')
+        .fadeIn()
+        .addClass('active');
     }, 1000);
-    $('.menu-open:checked ~ #camera, #microphone, #cog, #clock').fadeOut();
-    $('.menu-open-button').fadeOut();
+
+    $(`.menu-open:checked ~ #${menuItemId}`)
+      .removeClass('normal')
+      .addClass('wipe');
+    $(`.menu-open:checked ~ ${restSelector}`)
+      .fadeOut();
+    $('.menu-open-button')
+      .fadeOut();
+
     $('.menu-open-button').on('click', () => {
-      $('.menu-open:checked ~ #pencil').removeClass('wipe').addClass('normal');
-      $('.menu-open:checked ~ #pencil').removeClass('finished');
-      $('.menu-open-button').removeClass('active');
-      $('.menu-open:checked ~ #camera, #microphone, #cog, #clock').fadeIn();
+      $(`.menu-open:checked ~ #${menuItemId}`)
+        .removeClass('wipe')
+        .removeClass('finished')
+        .addClass('normal');
+      $('.menu-open-button')
+        .removeClass('active');
+      $(`.menu-open:checked ~ ${restSelector}`)
+        .fadeIn();
     });
-  });
-  $('#camera').on('click', () => {
-    console.log('clicked');
-    $('.menu-open:checked ~ #camera').removeClass('normal').addClass('wipe');
-    setTimeout(() => {
-      $('.menu-open:checked ~ #camera').addClass('finished');
-      $('.menu-open-button').fadeIn().addClass('active');
-    }, 1000);
-    $('.menu-open:checked ~ #pencil, #microphone, #cog, #clock').fadeOut();
-    $('.menu-open-button').fadeOut();
-    $('.menu-open-button').on('click', () => {
-      $('.menu-open:checked ~ #camera').removeClass('wipe').addClass('normal');
-      $('.menu-open:checked ~ #camera').removeClass('finished');
-      $('.menu-open-button').removeClass('active');
-      $('.menu-open:checked ~ #pencil, #microphone, #cog, #clock').fadeIn();
-    });
-  });
-  $('#microphone').on('click', () => {
-    console.log('clicked');
-    $('.menu-open:checked ~ #microphone').addClass('wipe');
-    setTimeout(() => {
-      $('.menu-open:checked ~ #microphone').removeClass('normal').addClass('finished');
-      $('.menu-open-button').fadeIn().addClass('active');
-    }, 1000);
-    $('.menu-open:checked ~ #pencil, #camera, #cog, #clock').fadeOut();
-    $('.menu-open-button').fadeOut();
-    $('.menu-open-button').on('click', () => {
-      $('.menu-open:checked ~ #microphone').removeClass('wipe').addClass('normal');
-      $('.menu-open:checked ~ #microphone').removeClass('finished');
-      $('.menu-open-button').removeClass('active');
-      $('.menu-open:checked ~ #pencil, #camera, #cog, #clock').fadeIn();
-    });
-  });
-  $('#clock').on('click', () => {
-    console.log('clicked');
-    $('.menu-open:checked ~ #clock').removeClass('normal').addClass('wipe');
-    setTimeout(() => {
-      $('.menu-open:checked ~ #clock').addClass('finished');
-      $('.menu-open-button').fadeIn().addClass('active');
-      $('.notifications').addClass('active');
-    }, 1000);
-    $('.menu-open:checked ~ #pencil, #camera, #cog, #microphone').fadeOut();
-    $('.menu-open-button').fadeOut();
-    $('.menu-open-button').on('click', () => {
-      $('.menu-open:checked ~ #clock').removeClass('wipe').addClass('normal');
-      $('.menu-open:checked ~ #clock').removeClass('finished');
-      $('.menu-open-button, .notifications').removeClass('active');
-      $('.menu-open:checked ~ #pencil, #camera, #cog, #microphone').fadeIn();
-    });
-  });
-  $('#cog').on('click', () => {
-    console.log('clicked');
-    $('.menu-open:checked ~ #cog').removeClass('normal').addClass('wipe');
-    setTimeout(() => {
-      $('.menu-open:checked ~ #cog').addClass('finished');
-      $('.menu-open-button').fadeIn().addClass('active');
-      $('.settings').addClass('active');
-    }, 1000);
-    $('.menu-open:checked ~ #pencil, #camera, #clock, #microphone').fadeOut();
-    $('.menu-open-button').fadeOut();
-    $('.menu-open-button').on('click', () => {
-      $('.menu-open:checked ~ #cog').removeClass('wipe').addClass('normal');
-      $('.menu-open:checked ~ #cog').removeClass('finished');
-      $('.menu-open-button, .settings').removeClass('active');
-      $('.menu-open:checked ~ #pencil, #camera, #clock, #microphone').fadeIn();
+  }
+
+  menuItemIds.forEach((menuItemId) => {
+    $(`#${menuItemId}`).on('click', () => {
+      setMenuItemClick(menuItemId);
     });
   });
 });
