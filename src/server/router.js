d803e8e4d64f90b1c056a26c5a938b79a4637675
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-12 12:10:12 +0100

    Remove comments in code Relates #70

--- src/server/router.js
+++ src/server/router.js
@@ -9,9 +9,7 @@ router.use(bodyParser.urlencoded({
 }));
 
 router.get('/', require('./controllers/home.js'));
-// router.get('/sign-up', require('./controllers/sign-up.js'));
 router.post('/memory-input-text', require('./controllers/addTextMemory.js'));
-// router.post('/add-new-user', require('./controllers/addNewUser.js'));
 router.route('/memories')
   .get(require('./controllers/getMemories.js'))
   .delete(require('./controllers/deleteMemory.js'));
