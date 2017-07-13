dbc0ad7a104e77e3e3f5d52709e5a00d4b75ed16
pbywater <philippabywater@gmail.com>
2017-07-13 10:57:23 +0100

    files now caching
    relates to #84

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,6 @@
-const cacheName = 'echo-app';
+/* eslint-disable */
+
+const cacheName = 'echo-app-test';
 
 const filesToCache = [
   '/',
@@ -15,32 +17,36 @@ const filesToCache = [
   '/assets/icons/card_icons/photo_folder_icon.svg',
   '/scripts/main.js'];
 
-self.addEventListener('install', (e) => {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    }),
-  );
-});
+  self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
 
-self.addEventListener('activate', (e) => {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(keyList => Promise.all(keyList.map((key) => {
-      if (key !== cacheName) {
-        console.log('[ServiceWorker] Removing old cache', key);
-        return caches.delete(key);
-      }
-    }))),
-  );
-  return self.clients.claim();
-});
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+    return self.clients.claim();
+  });
 
-self.addEventListener('fetch', (e) => {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request)),
-  );
-});
+  self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
