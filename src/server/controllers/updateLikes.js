5ff70c42e9d58c49d568eef99cabfd10d94a45d4
pbywater <philippabywater@gmail.com>
2017-07-13 14:37:14 +0100

    fixing error handling in updateLikes
    relates to #71

--- src/server/controllers/updateLikes.js
+++ src/server/controllers/updateLikes.js
@@ -5,7 +5,9 @@ const { updateLikes } = require('./../../database/db_update');
 module.exports = (req, res) => {
   updateLikes(req.body.numLikes, req.body.memoryId, (error, response) => {
     if (error) {
-      if (error) console.log('update error is', error);
+      console.error('getLikes error is', error);
+      res.status(500).send(error);
+      return;
     }
     res.send(res.rows);
   });
