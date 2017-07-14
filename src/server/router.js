07f1fad9ab0988725802941122b5f9e537fc9002
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-14 15:19:03 +0100

    Add cookie-parser to router and add /logout route Relates #94

--- src/server/router.js
+++ src/server/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const router = express.Router();
 
@@ -7,8 +8,10 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true,
 }));
+router.use(cookieParser());
 
 router.get('/', require('./controllers/home.js'));
+router.post('/logout', require('./controllers/logout.js'));
 router.post('/memory-input-text', require('./controllers/addTextMemory.js'));
 router.post('/login', require('./controllers/login.js'));
 router.route('/memories')
