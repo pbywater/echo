1bf5a2d59b69128192bd48cda90a51eaa9adf0f6
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-14 11:27:25 +0100

    Add error status code if user cannot be created Relates #79

--- src/server/controllers/addNewUser.js
+++ src/server/controllers/addNewUser.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
   createUser(req.body, (error, response) => {
     if (error) {
       // will be refactored, so that err.message is displayed on screen
-      res.send({ error: error.message });
+      res.status(400).send({ error: error.message });
     }
     res.redirect('/');
   });
