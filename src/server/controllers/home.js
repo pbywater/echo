d685c6b630b1a13c7415d265518f1713debb57f0
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-14 17:39:02 +0100

    updated file path relates #100

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -3,5 +3,5 @@ const path = require('path');
 
 module.exports = (req, res) => {
   // path.join(__dirname, '..', 'public', 'index.html');
-  res.sendFile('/index.html');
+  res.sendFile('/public/index.html');
 };
