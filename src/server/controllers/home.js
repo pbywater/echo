999362b6504e433734af3bac96dee6a2647ae35f
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-14 17:27:52 +0100

    switched devdep to dep because heroku apparently doesnt like devdeps. worth a try relates #100

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -2,5 +2,7 @@ const express = require('express');
 const path = require('path');
 
 module.exports = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
   path.join(__dirname, '..', 'public', 'index.html');
 };
