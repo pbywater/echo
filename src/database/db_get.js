26759f35c7bb4bfdbc826258fb79ec5b9ee4f9a2
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-14 17:10:19 +0100

    Change sql db query so that we are selecting the id that corresponds to the users id Relates #98

--- src/database/db_get.js
+++ src/database/db_get.js
@@ -1,17 +1,21 @@
 const connect = require('./db_connect');
 
-const getMemories = (userId, callback) => {
-  connect.query(
-    `SELECT memories.id, memories.heading, memories.likes, memories.visits, memories.tag, memories.memory_asset_url, memories.memory_text, memories.media_type
-    FROM memories
-    INNER JOIN users
-    ON users.id = memories.user_id
-    WHERE users.id = $1;`, [userId], (err, res) => {
-      if (err) {
-        return callback(err);
-      }
-      return callback(null, res); // will be refactored
-    });
+const getMemories = (userLogin, callback) => {
+  const userSQLMemories =
+  `SELECT memories.id, memories.heading, memories.likes, memories.visits,
+  memories.tag, memories.memory_asset_url, memories.memory_text,
+  memories.media_type
+  FROM memories
+  INNER JOIN users
+  ON users.id = memories.user_id
+  WHERE users.id = (SELECT users.id FROM users WHERE username = $1 OR email = $1)`;
+
+  connect.query(userSQLMemories, [userLogin], (err, res) => {
+    if (err) {
+      return callback(err);
+    }
+    return callback(null, res);
+  });
 };
 
 const userSQL = 'SELECT * FROM users WHERE username = $1 OR email = $1';
