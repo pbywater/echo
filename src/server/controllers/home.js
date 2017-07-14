929b6b9b13e0e5f89ed4e82ab7f650cf54c3fcbe
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-14 17:54:18 +0100

    changed home route relates #100

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -2,6 +2,5 @@ const express = require('express');
 const path = require('path');
 
 module.exports = (req, res) => {
-  // path.join(__dirname, '..', 'public', 'index.html');
-  res.sendFile('/public/index.html');
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 };
