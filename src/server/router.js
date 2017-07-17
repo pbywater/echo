f9b25588339ee0d3cab4fe6011f628a85b9869e4
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-17 17:10:28 +0100

    Require cookie-session in router and create session for authenticated Relates #94

--- src/server/router.js
+++ src/server/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 
 const router = express.Router();
 
@@ -8,7 +8,21 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true,
 }));
-router.use(cookieParser());
+
+const sessionSecret = '3294r4whasuodifhd13urde80sufj138ryt9-gu538re9ghvisujela4hrsf4rue5r89';
+
+const session = cookieSession({
+  name: 'authenticated',
+  secret: sessionSecret,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    signed: true,
+    maxAge: 1 * 60 * 60 * 1000, // 1 hour
+  },
+});
+
+router.use(session);
 
 router.get('/', require('./controllers/home.js'));
 router.post('/logout', require('./controllers/logout.js'));
