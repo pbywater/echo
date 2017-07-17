0a781e413dab7c967d747bfa85d2862bd5fbf7d9
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-17 12:38:50 +0100

    Remove console log from home.js Relates #100

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -2,7 +2,5 @@ const express = require('express');
 const path = require('path');
 
 module.exports = (req, res) => {
-  // res.sendFile(`${__dirname}public` + 'index.html');
-  console.log(__dirname);
   res.sendFile('../../../public/index.html', { root: __dirname });
 };
