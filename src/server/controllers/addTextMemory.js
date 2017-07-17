199277cd763b550ceb5f1ee88c75f3b1500d5c5c
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-17 11:58:03 +0100

    removed unnec comments relates #106

--- src/server/controllers/addTextMemory.js
+++ src/server/controllers/addTextMemory.js
@@ -5,8 +5,6 @@ const { createMemory } = require('./../../database/db_create');
 
 module.exports = (req, res) => {
   createMemory(req.body, 'text_only', (error, response) => {
-      // 1 hard coded in at the moment.
-      // We will later add the userId here to get the relevant memories.
     if (error) return res(error);
     setTimeout(() => {
       res.redirect('/');
