e439822f053183616efb4ba84702b279d741467f
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-17 10:24:35 +0100

    updating file path relates #100

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -2,5 +2,5 @@ const express = require('express');
 const path = require('path');
 
 module.exports = (req, res) => {
-  res.sendFile('./public/index.html');
+  res.sendFile(`${__dirname}public` + 'index.html');
 };
