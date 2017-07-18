dcf0e3a79d2ecbc133faa05f8a0b730a7ee41c2e
pbywater <philippabywater@gmail.com>
2017-07-18 17:17:02 +0100

    adding xhr requests
    relates to #116

---
+++ public/scripts/s3-uploads.js
@@ -0,0 +1,41 @@
+(function () {
+  document.getElementById('camera').onchange = function () {
+    const files = document.getElementById('camera').files;
+    const file = files[0];
+    if (file == null) {
+      return alert('No file selected.');
+    }
+    getSignedRequest(file);
+  };
+}());
+
+function getSignedRequest(file) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        uploadFile(file, response.signedRequest, response.url);
+      } else {
+        alert('Could not get signed URL.');
+      }
+    }
+  };
+  xhr.send();
+}
+
+function uploadFile(file, signedRequest, url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('PUT', signedRequest);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        document.getElementById('camera').src = url;
+      } else {
+        alert('Could not upload file.');
+      }
+    }
+  };
+  xhr.send(file);
+}
