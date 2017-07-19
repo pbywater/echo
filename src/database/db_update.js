6cf86a71d48ae7af7fe3b58d22ff258c71df7c9c
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-19 15:16:04 +0100

    Add updatePhotoMemory function to update tag and heading in db Relates #116

--- src/database/db_update.js
+++ src/database/db_update.js
@@ -5,6 +5,15 @@ const updateLikes = (likeNum, memoryId, cb) => {
     'UPDATE memories SET likes = $1 WHERE memories.id = $2', [likeNum, memoryId], cb);
 };
 
+const updatePhotoMemory = (userId, tag, heading, imageId, cb) => {
+  connect.query(
+    `UPDATE memories
+     SET tag = $2, heading = $3
+     WHERE user_id = (SELECT users.id FROM users WHERE username = $1 OR email = $1)
+     AND memory_asset_url = $4`, [userId, tag, heading, imageId], cb);
+};
+
 module.exports = {
   updateLikes,
+  updatePhotoMemory,
 };
