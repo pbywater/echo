67e6d96520065bb64b006b9b13563782154e84b0
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-19 16:18:34 +0100

    Change test data in db build so that images are stored in memory_asset_url Relates #116

--- database_build/db_build.sql
+++ database_build/db_build.sql
@@ -18,7 +18,7 @@ CREATE TABLE memories (
   heading           VARCHAR(50),
   likes             INTEGER,
   visits            INTEGER,
-  tag               VARCHAR(50),                    
+  tag               VARCHAR(50),
   memory_asset_url  VARCHAR(500),
   memory_text       VARCHAR(2000),
   media_type        media_type
@@ -30,19 +30,19 @@ VALUES ('test', 'will be hashed', 'test@test.com');
 INSERT INTO memories (user_id, heading, likes, visits, tag, memory_asset_url, memory_text, media_type)
 VALUES
 (1, 'testHeading', 10, 1, 'family', '', 'Donec et orci eu felis malesuada venenatis. Aliquam erat volutpat. Morbi eget felis ut metus malesuada ullamcorper. Maecenas sem nulla, tincidunt ut aliquam vestibulum, iaculis vel diam. Morbi mattis leo.', 'text_only'),
-(1, 'testHeading', 7, 1, 'family', 'testurl', 'testMemoryText', 'audio'),
-(1, 'testHeading', 35, 1, 'family', 'https://levoyageverslest.files.wordpress.com/2010/01/zaoouunautre.jpg', 'testMemoryText', 'image'),
-(1, 'testHeading', 7, 1, 'family', 'testurl', 'testMemoryText', 'video'),
+(1, 'testHeading', 7, 1, 'family', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 35, 1, 'family', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 7, 1, 'family', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
 (1, 'testHeading', 19, 1, 'family', '', 'Donec et orci eu felis malesuada venenatis. Aliquam erat volutpat. Morbi eget felis ut metus malesuada ullamcorper. Maecenas sem nulla, tincidunt ut aliquam vestibulum, iaculis vel diam. Morbi mattis leo.', 'text_only'),
-(1, 'testHeading', 10, 1, 'pets', 'testurl', 'testMemoryText', 'audio'),
-(1, 'testHeading', 7, 1, 'pets', 'https://levoyageverslest.files.wordpress.com/2010/01/zaoouunautre.jpg', 'testMemoryText', 'image'),
-(1, 'testHeading', 35, 1, 'pets', 'testurl', 'testMemoryText', 'video'),
+(1, 'testHeading', 10, 1, 'pets', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 7, 1, 'pets', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 35, 1, 'pets', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
 (1, 'testHeading', 7, 1, 'pets', '', 'Donec et orci eu felis malesuada venenatis. Aliquam erat volutpat. Morbi eget felis ut metus malesuada ullamcorper. Maecenas sem nulla, tincidunt ut aliquam vestibulum, iaculis vel diam. Morbi mattis leo.', 'text_only'),
-(1, 'testHeading', 19, 1, 'pets', 'testurl', 'testMemoryText', 'audio'),
-(1, 'testHeading', 10, 1, 'friends', 'https://levoyageverslest.files.wordpress.com/2010/01/zaoouunautre.jpg', 'testMemoryText', 'image'),
-(1, 'testHeading', 7, 1, 'friends', 'testurl', 'testMemoryText', 'video'),
+(1, 'testHeading', 19, 1, 'pets', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 10, 1, 'friends', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 7, 1, 'friends', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
 (1, 'testHeading', 35, 1, 'friends', '', 'Donec et orci eu felis malesuada venenatis. Aliquam erat volutpat. Morbi eget felis ut metus malesuada ullamcorper. Maecenas sem nulla, tincidunt ut aliquam vestibulum, iaculis vel diam. Morbi mattis leo.', 'text_only'),
-(1, 'testHeading', 6, 1, 'friends', 'testurl', 'testMemoryText', 'audio'),
-(1, 'testHeading', 19, 1, 'friends', 'https://levoyageverslest.files.wordpress.com/2010/01/zaoouunautre.jpg', 'testMemoryText', 'image');
+(1, 'testHeading', 6, 1, 'friends', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image'),
+(1, 'testHeading', 19, 1, 'friends', 'cj5b4injc0000o0rrej35hdni.cat.jpg', 'testMemoryText', 'image');
 
 COMMIT;
