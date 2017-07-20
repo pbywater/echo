8b97fcea87d80741e01362071dbad815570b6080
pbywater <philippabywater@gmail.com>
2017-07-20 11:04:06 +0100

    rendering newUser sequence in our render function relates to #108

--- src/client/d3/d3.js
+++ src/client/d3/d3.js
@@ -7,17 +7,17 @@ const { newUserIntro } = require('./newUserIntro.js');
 const url = location.hostname ? '/memories' : jsonUrl;
 
 d3.json(url, (err, data) => {
-  if (data) {
-    // constructTagList(data);
+  if (data.length > 0) {
+    constructTagList(data);
     render(formatData(data));
+    initTagMenu();
   } else {
     const falseDataArray = [{ heading: 'test', id: 100, index: 0, likes: 5, visits: 1, x: 215, y: 170 }];
-    const falseProcessedData = { links: [], nodes: {} };
-    render(falseProcessedData, falseDataArray);
+    const falseProcessedData = { links: [], nodes: falseDataArray };
+    render(falseProcessedData);
     newUserIntro();
   }
-  // initTagMenu();
-  // initSubmitMemory();
+  initSubmitMemory();
 });
 
 const formatData = (data) => {
@@ -54,6 +54,7 @@ function render(updatedData) {
   .data(updatedData.links, d => d.target.id);
 
   let nodes = nodeGrp
+  .attr('class', 'nodeGroup')
   .selectAll('circle.memory')
   .data(nodeDataArray, d => d.id);
 
