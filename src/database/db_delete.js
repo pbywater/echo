fb5fccb9fb59492a106d3ea63296a4ba9cb2925d
pbywater <philippabywater@gmail.com>
2017-07-20 17:04:42 +0100

    removing logs
    relates to #138

--- src/database/db_delete.js
+++ src/database/db_delete.js
@@ -4,9 +4,7 @@ const sqlQuery = 'DELETE FROM memories WHERE memories.id = $1;';
 
 const deleteMemory = (memoryId, callback) => {
   connect.query(sqlQuery, [memoryId], (err) => {
-    console.log('memoryid is ', memoryId);
     if (err) {
-      console.log('err is ', err);
       return callback(err);
     }
     callback(null);
--- src/server/controllers/deleteMemory.js
+++ src/server/controllers/deleteMemory.js
@@ -5,8 +5,6 @@ const { deleteMemory } = require('./../../database/db_delete');
 module.exports = (req, res) => {
   deleteMemory(req.body.id, (error, response) => {
     if (error) return res.status(500).send(error);
-    console.log('response is ', response);
-    console.log('error is ', error);
     res.status(200).send('ok');
   });
 };
