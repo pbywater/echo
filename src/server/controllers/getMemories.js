01016a3a8fed69fa0563ba4afe3107e12ac8c901
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-20 12:01:34 +0100

    Pass in cookie info to getMemories so that users own memories are fetched from db relates #130

--- src/server/controllers/getMemories.js
+++ src/server/controllers/getMemories.js
@@ -1,9 +1,9 @@
-const express = require('express');
-require('env2')('./config.env');
+// const express = require('express');
+// require('env2')('./config.env');
 const { getMemories } = require('./../../database/db_get');
 
 module.exports = (req, res) => {
-  getMemories('test', (error, memories) => {
+  getMemories(req.session.name, (error, memories) => {
     if (error) {
       res.send(error);
       return;
