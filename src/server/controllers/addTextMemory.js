689da3bad74a45b8b4e1dcca47e31c2b4186c427
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-20 12:00:52 +0100

    Pass in cookie info to createMemory function so that text memory is created with correct user Relates #130

--- src/server/controllers/addTextMemory.js
+++ src/server/controllers/addTextMemory.js
@@ -4,8 +4,7 @@ require('env2')('./config.env');
 const { createMemory } = require('./../../database/db_create');
 
 module.exports = (req, res) => {
-  createMemory(req.cookies.name, req.body, 'text_only', (error, response) => {
-
+  createMemory(req.session.name, req.body, 'text_only', (error, response) => {
     if (error) return res(error);
     setTimeout(() => {
       res.redirect('/');
