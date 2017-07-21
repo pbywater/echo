2871447dd3e1b8b08365ebb4022d258818d040f7
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-21 16:53:13 +0100

    Pass req.session.id in to createMemory relates #152

--- src/server/controllers/addTextMemory.js
+++ src/server/controllers/addTextMemory.js
@@ -4,7 +4,7 @@ require('env2')('./config.env');
 const { createMemory } = require('./../../database/db_create');
 
 module.exports = (req, res) => {
-  createMemory(req.cookies.name, req.body, 'text_only', 'null', (error, response) => {
+  createMemory(req.session.id, req.body, 'text_only', 'null', (error, response) => {
     if (error) return res(error);
     setTimeout(() => {
       res.redirect('/');
