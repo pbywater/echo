146747ce33deb98a804b264731713f117bc56e87
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-21 16:52:25 +0100

    Pass id in to createMemory as id is now available in req.session relates #152

--- src/database/db_create.js
+++ src/database/db_create.js
@@ -2,15 +2,14 @@ const connect = require('./db_connect');
 
 const hashPassword = require('./../helpers/hashPassword');
 
-const createMemory = (login, newMemory, mediaType, memoryUrl, callback) => {
+const createMemory = (id, newMemory, mediaType, memoryUrl, callback) => {
   connect.query(
     `INSERT INTO memories
         (user_id, memory_text, media_type, memory_asset_url, heading, tag, likes, visits)
      VALUES
-        ((SELECT users.id FROM users WHERE username = $1 OR email = $1),
-          $2, $3, $4, $5, $6, $7, $8)
+        ($1, $2, $3, $4, $5, $6, $7, $8)
     RETURNING id;`,
-    [login, newMemory.memory_text, mediaType, memoryUrl, newMemory.heading, newMemory.tag, 0, 0],
+    [id, newMemory.memory_text, mediaType, memoryUrl, newMemory.heading, newMemory.tag, 0, 0],
 
     (err, res) => {
       if (err) { callback(err); }
