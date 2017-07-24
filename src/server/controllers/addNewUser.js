b99129b7b7cd04eb073855e4171b2434bddee2c3
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-24 14:46:48 +0100

    added random token generator and a token expiration time relates #133

--- src/server/controllers/addNewUser.js
+++ src/server/controllers/addNewUser.js
@@ -1,6 +1,20 @@
 const { createUser } = require('./../../database/db_create');
 
 module.exports = (req, res) => {
+  // create random 16 character token
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 16; i > 0; --i) {
+    token += chars[Math.round(Math.random() * (chars.length - 1))];
+  }
+
+  // create expiration date
+  const expires = new Date();
+  expires.setHours(expires.getHours() + 6);
+
+  req.body.token = token;
+  req.body.expires = expires;
+
   createUser(req.body, (error, response) => {
     if (error) {
       // will be refactored, so that err.message is displayed on screen
