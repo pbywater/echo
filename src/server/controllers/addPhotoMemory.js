be8980f754f4fd45f54d84025f68fff40e053304
Joey Scott <joannascott29@hotmail.co.uk>
2017-07-24 17:53:27 +0100

    Send error or success status codes in callback in updatePhotoMemory relates #116

--- src/server/controllers/addPhotoMemory.js
+++ src/server/controllers/addPhotoMemory.js
@@ -2,7 +2,8 @@ const { updatePhotoMemory } = require('./../../database/db_update');
 
 module.exports = (req, res) => {
   updatePhotoMemory(req.session.id, req.body.tag, req.body.heading, req.body.imageId,
-    (err, res) => {
-      if (err) return err;
+    (error, response) => {
+      if (error) return res.status(500).send(error);
+      res.status(200).send('ok');
     });
 };
