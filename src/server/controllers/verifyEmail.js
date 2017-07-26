32a6684f1d03e2dbb858c9a25d0d8f6a099b6509
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-26 14:54:14 +0100

    sent the client a 500 if error relates #133

--- src/server/controllers/verifyEmail.js
+++ src/server/controllers/verifyEmail.js
@@ -3,7 +3,7 @@ const { updateEmailVerification } = require('./../../database/db_update');
 module.exports = (req, res) => {
 
  updateEmailVerification(req.params.username, req.params.token, (error, response) => {
-   if (error) { return error }; //TODO: handle error
+   if (error) { res.status(500).send(error) };
    res.redirect('/');
  })
 };
