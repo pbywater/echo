a8876283c667181a254f5f9e651133e29fc79d85
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-28 16:09:18 +0100

    updated error handling in controller relates #133

--- src/server/controllers/verifyEmail.js
+++ src/server/controllers/verifyEmail.js
@@ -3,7 +3,7 @@ const { updateEmailVerification } = require('./../../database/db_update');
 module.exports = (req, res) => {
 
  updateEmailVerification(req.params.username, req.params.token, (error, response) => {
-   if (error) { res.status(500).send(error) };
+   if (err) return res.send(err);
    res.redirect('/');
- })
+  })
 };
