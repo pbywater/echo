3fc99b43fca4843e63f54894d79094208208618b
Joey Scott <joannascott29@hotmail.co.uk>
2017-08-01 15:35:13 +0100

    Refactor /memories delete route so that delete memories is called inside of getMemoryById relates #116 #125

--- src/server/controllers/assetDeleteUrl.js
+++ src/server/controllers/assetDeleteUrl.js
@@ -8,28 +8,33 @@ const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 const s3 = s3init();
 
 module.exports = (req, res) => {
-  getMemoryById(req.session.id, req.body.id, (error, response) => {
-    if (error) return error;
+  getMemoryById(req.session.id, req.body.id, (err, response) => {
+    if (err) return err;
 
-    const params = {
-      Bucket: S3_BUCKET_NAME,
-      Delete: {
-        Objects: [
-          {
-            Key: response,
-          },
-        ],
-      },
-    };
+    if (response.media_type === 'image' || response.media_type === 'audio') {
+      const s3key = response.memory_asset_url;
 
-    s3.deleteObjects(params, (err, data) => {
-      if (err) console.log(err);
-      else console.log(data);
+      const params = {
+        Bucket: S3_BUCKET_NAME,
+        Delete: {
+          Objects: [
+            {
+              Key: s3key,
+            },
+          ],
+        },
+      };
+
+      s3.deleteObjects(params, (err, data) => {
+        if (err) return err;
+      });
+    }
+
+    deleteMemory(req.session.id, req.body.id, (error) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      res.status(200).send('ok');
     });
-  });
-
-  deleteMemory(req.body.id, (error, response) => {
-    if (error) return res.status(500).send(error);
-    res.status(200).send('ok');
   });
 };
