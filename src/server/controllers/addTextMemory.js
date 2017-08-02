6b591a0205970e19687b85564ca47c55d958d504
pbywater <philippabywater@gmail.com>
2017-08-02 15:30:25 +0100

    acting as test user and fixing error in sign up to continue
    relates to #138

--- src/server/controllers/addTextMemory.js
+++ src/server/controllers/addTextMemory.js
@@ -1,7 +1,8 @@
 const { createMemory } = require('./../../database/db_create');
 
 module.exports = (req, res) => {
-  createMemory(req.session.id, req.body, 'text_only', 'null', (error, response) => {
+  console.log('session id', req);
+  createMemory('test', req.body, 'text_only', 'null', (error, response) => {
     if (error) return res.status(500).send(error);
     res.status(200).send('ok');
   });
