8efc2ec36a5d7d80fa2fe75881b15ade7eeeb4b8
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-08-02 16:28:56 +0100

    initialise notifications relates #169

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -72,6 +72,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 //notifications stuff below:
 
+  var pushButton = document.querySelector('.js-push-btn');
+
   if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker and Push is supported');
 
@@ -80,6 +82,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       console.log('Service Worker is registered', swReg);
 
       swRegistration = swReg;
+      initialiseUI();
     })
     .catch(function(error) {
       console.error('Service Worker Error', error);
@@ -90,5 +93,93 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
 
   const applicationServerPublicKey = 'BPJmORLwdaOx2QAnX1fYEUjDVs9qyVCCjZhTKkSWqFbi5uQ0-8Ovxf998AGacEhnG6VIk46E-jfQ-l5ycoVvPQk';
+
+  function initialiseUI() {
+    pushButton.addEventListener('click', function() {
+      pushButton.disabled = true;
+      if (isSubscribed) {
+        // TODO: Unsubscribe user
+      } else {
+        subscribeUser();
+      }
+    });
+
+    // Set the initial subscription value
+    swRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+      isSubscribed = !(subscription === null);
+
+      // updateSubscriptionOnServer(subscription);
+
+      if (isSubscribed) {
+        console.log('User IS subscribed.');
+      } else {
+        console.log('User is NOT subscribed.');
+      }
+
+      updateBtn();
+    });
+  }
+
+  function updateBtn() {
+    if (isSubscribed) {
+      pushButton.textContent = 'Disable Push Messaging';
+    } else {
+      pushButton.textContent = 'Enable Push Messaging';
+    }
+
+    pushButton.disabled = false;
+  }
+
+  function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(subscription) {
+      console.log('User is subscribed.');
+
+      updateSubscriptionOnServer(subscription);
+
+      isSubscribed = true;
+
+      updateBtn();
+    })
+    .catch(function(err) {
+      console.log('Failed to subscribe the user: ', err);
+      updateBtn();
+    });
+  }
+
+  function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to application server
+
+    const subscriptionJson = document.querySelector('.js-subscription-json');
+    const subscriptionDetails =
+      document.querySelector('.js-subscription-details');
+
+    if (subscription) {
+      subscriptionJson.textContent = JSON.stringify(subscription);
+      subscriptionDetails.classList.remove('is-invisible');
+    } else {
+      subscriptionDetails.classList.add('is-invisible');
+    }
+  }
+
+  function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
 
 });
