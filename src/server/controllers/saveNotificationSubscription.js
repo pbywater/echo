c00ddb21e6670d592b05c5949e3ecd4eaa9b6920
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-08-03 16:13:25 +0100

    set up web push relates #169

--- src/server/controllers/saveNotificationSubscription.js
+++ src/server/controllers/saveNotificationSubscription.js
@@ -1,15 +1,34 @@
 const { updateNotificationSubscription } = require('./../../database/db_update');
 
 module.exports = (req, res) => {
-  // console.log('req.body is', req.body);
 // req.session is currently empty. This will be fixed. Hardcoding test at the moment.
-// console.log(typeof );
+
+  if (!req.body || !req.body.endpoint) {
+    // Not a valid subscription.
+    res.status(400);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      error: {
+        id: 'no-endpoint',
+        message: 'Subscription must have an endpoint.'
+      }
+    }));
+  }
+
   updateNotificationSubscription(JSON.stringify(req.body), 'test', (err, result) => {
     if (err) {
-      res.status(400).send({ err: err.message });
-      return
+      res.status(500);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({
+        error: {
+          id: 'unable-to-save-subscription',
+          message: 'The subscription was received but we were unable to save it to our database.'
+        }
+      }));
+      return;
     }
-    // res.redirect('/');
-    res.status(200).send('ok');
+    // res.status(200).send('ok');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ data: { success: true } }));
   })
 }
