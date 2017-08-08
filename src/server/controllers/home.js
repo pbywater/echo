7ccef07fcf8d3d4c54ed3221ea5ca9109a265efe
Joey Scott <joannascott29@hotmail.co.uk>
2017-08-08 13:24:12 +0100

    Add req.session.id to if statement in home route relates #130

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = (req, res) => {
-  if (req.session.name) {
+  if (req.session.id || req.session.name) {
     res.sendFile(path.join(__dirname, '../../..', 'public', 'index.html'));
   } else {
     res.sendFile(path.join(__dirname, '../../..', 'public', 'signup.html'));
