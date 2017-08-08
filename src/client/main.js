73c2132ced7e0a81dd3c15c24ee2aad13ba52ac4
Joey Scott <joannascott29@hotmail.co.uk>
2017-08-08 13:49:59 +0100

    Require in all files within public/scripts so that they are included in browserify command relates #181

--- src/client/main.js
+++ src/client/main.js
@@ -1,4 +1,8 @@
 require('./d3/d3.js');
+require('./../../public/scripts/main.js');
+require('./../../public/scripts/s3-helpers.js');
+require('./../../public/scripts/s3-uploads.js');
+require('./../../public/scripts/sign-up.js');
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
