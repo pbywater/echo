96665aede7dce2a31b3350c601ee315a20673e8f
Joey Scott <joannascott29@hotmail.co.uk>
2017-08-21 16:44:00 +0100

    Add if statement so that if not logged in, login page is rendered, otherwise render home relates #191

--- src/server/controllers/sign-up.js
+++ src/server/controllers/sign-up.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = (req, res) => {
-
-  res.sendFile(path.join(__dirname, '../../..', 'public', 'signup.html'));
-
+  if (!req.session.id && !req.session.name) {
+    res.sendFile(path.join(__dirname, '../../..', 'public', 'signup.html'));
+  } else {
+    res.redirect('/');
+  }
 };
