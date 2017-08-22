8b93260c0d58f9d4537c2a0ce95727073cef20c3
Joey Scott <joannascott29@hotmail.co.uk>
2017-08-22 17:06:17 +0100

    Add functions that call fetch request when login button is clicked and display error message if required relates #204

---
+++ public/scripts/loginValidation.js
@@ -0,0 +1,40 @@
+$('#submit-login').on('click', ((e) => {
+  e.preventDefault();
+  const data = $('.login-form').serialize();
+  fetch('POST', 'login', data, handleFetchOutput);
+}),
+);
+
+function fetch(method, url, data, handleResponseCallback) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        handleResponseCallback(null, 'login correct');
+      } else if (xhr.status === 400) {
+        const jsonObj = JSON.parse(xhr.responseText);
+        handleResponseCallback(jsonObj);
+      }
+    }
+  };
+  xhr.open(method, url, true);
+  xhr.send(data);
+}
+
+function handleFetchOutput(err, data) {
+  if (err) {
+    loginError(err);
+  } else {
+    window.location.replace('/');
+  }
+}
+
+function loginError(errorMessage) {
+  $('.error-login')
+    .text(`**${errorMessage}`)
+    .css('color', '#ff3f56')
+    .css('padding', '1em')
+    .css('padding-top', '0')
+    .show();
+}
