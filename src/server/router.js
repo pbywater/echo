4069f55cb2efb6c93fa4f67b20fe05bbc37899cc
Joey Scott <joannascott29@hotmail.co.uk>
2017-08-22 14:22:06 +0100

    Add routes for password reset relates #213

--- src/server/router.js
+++ src/server/router.js
@@ -17,7 +17,7 @@ const session = cookieSession({
   secret: process.env.SESSION_SECRET,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true, //we'll need to change this to https when we officially launch on heroku
+    httpOnly: true, // we'll need to change this to https when we officially launch on heroku
     signed: true,
     maxAge: 1 * 60 * 60 * 1000, // 1 hour
   },
@@ -36,6 +36,9 @@ router.post('/add-new-user', require('./controllers/addNewUser.js'));
 router.route('/login')
   .get(require('./controllers/sign-up.js'))
   .post(require('./controllers/login.js'));
+router.route('/password_reset')
+  .get(require('./controllers/getPasswordReset.js'))
+  .post(require('./controllers/postPasswordReset.js'));
 router.get('/sign-s3', checkSignIn, require('./controllers/assetCreateUrl.js'));
 router.route('/memories')
   .get(checkSignIn, require('./controllers/getMemories.js'))
